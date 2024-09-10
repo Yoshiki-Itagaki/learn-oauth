f332cbbccd1b7483ac311a02df03c5bff934c7d1
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-11 00:08:03 +0900

    Created a Spring Boot project for OAuth with Spring Security

---
+++ src/test/java/com/in28minutes/learn_oauth/LearnOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
